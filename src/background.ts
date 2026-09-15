bb8b4acbaa8231e057ce90f94e43aca7b7bfa849
Keizou Wang <keizouw@proton.me>
2026-09-15 08:27:40 -0700

    underline unused sender parameter

--- src/background.ts
+++ src/background.ts
@@ -3,7 +3,7 @@ import { getSettings, setSettings } from "./lib/storage";
 
 const events: { [key: string]: Function } = { sync, getSettings, setSettings };
 
-chrome.runtime.onMessage.addListener(async function ({ event, payload }: Message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(async function ({ event, payload }: Message, _, sendResponse) {
 	if (event in events) sendResponse(await events[event](payload));
 	return true;
 });
